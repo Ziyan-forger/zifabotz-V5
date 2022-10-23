482badf45ec77f2b6e5680eeb79e888535337d8a
Ziyan-forger <111504164+Ziyan-forger@users.noreply.github.com>
2022-10-23 20:05:50 +0700

    Update belibot.js

--- plugins/belibot.js
+++ plugins/belibot.js
@@ -3,16 +3,16 @@ let { MessageType } = require('@adiwajshing/baileys')
 let handler = async(m, { conn }) => {
     let kamisato = `
 ╭═══ 〔 𝐋𝐈𝐒𝐓 𝐁𝐄𝐑𝐋𝐀𝐍𝐆𝐆𝐀𝐍𝐀𝐍 〕 ════
-├⬡ 𝟏 ᗷᑌᒪᗩᑎ
-├⬡ 4 ᗷᑌᒪᗩᑎ
-├⬡ 8 BULAN
+├⬡ 𝟏 Minggu 
+├⬡ 3 Minggu
+├⬡ 8 Minggu
 └═══════════════
 𝗦𝗶𝗹𝗮𝗵𝗸𝗮𝗻 𝗸𝗹𝗶𝗸 𝗽𝗮𝗱𝗮 "𝗟𝗶𝘀𝘁 𝗛𝗮𝗿𝗴𝗮" 𝘂𝗻𝘁𝘂𝗸 𝗺𝗲𝗹𝗶𝗵𝗮𝘁 𝗹𝗶𝘀𝘁.
 ╭═══════════════════════
 ║╭──❉ 〔 𝐕𝐈𝐀 𝐌𝐎𝐁𝐈𝐋𝐄 〕 ❉──────
-║│➸ *DANA* : 085828764046
-║│➸ *PULSA*: 085828764046
-║│➸ *GOPAY*: 085828764046
+║│➸ *DANA* : 085714056133
+║│➸ *PULSA*: 085714056133
+║│➸ *GOPAY*: 085714056133
 ║╰─────────
 ▌│█║▌║▌║║▌║▌║█│▌▌│█║▌║▌║║▌║▌║█│
 ╰═══════════════════════
@@ -21,9 +21,9 @@ let handler = async(m, { conn }) => {
         buttonText: 'List Harga',
         description: kamisato,
         sections:  [{title: "Silahkan di pilih", rows: [
-        {title: '4 Bulan', description: "Rp30.000\nSewa bot selama 4 bulan.", rowId:".payment"},
-        {title: '1 Bulan', description: "Rp15.000\nSewa bot selama 1 bulan.", rowId:".payment"},
-        {title: '8 bulan', description: "Rp40.000\nSewa bot selama 8 bulan.", rowId:".payment"},
+        {title: '4 Bulan', description: "Rp14.000\nSewa bot selama 1 minggu.", rowId:".payment"},
+        {title: '1 Bulan', description: "Rp19.000\nSewa bot selama 3 minggu.", rowId:".payment"},
+        {title: '8 bulan', description: "Rp30.000\nSewa bot selama 8 minggu.", rowId:".payment"},
         {title: 'Owner', description: "Chat owner nya jika ingin sewa bot.", rowId:".owner"},
         {title: 'Rules', description: "Kebijakan Privasi, Syarat Ketentuan dan Peraturan.", rowId:".rules"},
        ] }],
